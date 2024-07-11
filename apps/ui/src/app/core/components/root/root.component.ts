54c16b6d572ee32140465409901f5aed5914624e
NoNamer777 <oscar.wellner@gmail.com>
2024-07-11 14:39:54 +0200

    Run Prettier

--- apps/ui/src/app/core/components/root/root.component.ts
+++ apps/ui/src/app/core/components/root/root.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './root.component.html',
-  styleUrl: './root.component.scss',
-  standalone: true,
+    selector: 'app-root',
+    templateUrl: './root.component.html',
+    styleUrl: './root.component.scss',
+    standalone: true,
 })
 export class RootComponent {}
