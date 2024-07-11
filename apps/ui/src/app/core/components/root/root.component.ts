06f13ac9f24b0e4f9c7e51690e9a0949f280915c
NoNamer777 <oscar.wellner@gmail.com>
2024-07-11 14:45:47 +0200

    Add HeaderComponent

--- apps/ui/src/app/core/components/root/root.component.ts
+++ apps/ui/src/app/core/components/root/root.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header';
 
 @Component({
     selector: 'app-root',
     templateUrl: './root.component.html',
     styleUrl: './root.component.scss',
     standalone: true,
+    imports: [HeaderComponent],
 })
 export class RootComponent {}
